c26d04fcdaddbfb35cbf9d56d7e9cd6d624a548c
Nate <natesvoboda64@gmail.com>
2021-12-07 21:37:29 -0500

    final for gladiator battle

--- assets/js/game.js
+++ assets/js/game.js
@@ -158,6 +158,22 @@ var startGame = function() {
 var endGame = function() {
   window.alert("The game has now ended. Let's see how you did!");
 
+  // check localStorage for high score, if it's not there, use 0
+  var highScore = localStorage.getItem("highscore");
+  if (highScore === null){
+    highScore = 0;
+  }
+  // if player has more money than the high score, player has new high score
+  if (playerInfo.money > highScore){
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
+
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+  }
+  else {
+    alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+  }
+
   // if player is still alive, player wins!
   if (playerInfo.health > 0) {
     window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + '.');
@@ -206,10 +222,6 @@ shopOptionPrompt = parseInt(shopOptionPrompt);
   }
 };
 
-/* END GAME FUNCTIONS */
-
-/* GAME INFORMATION / VARIABLES */
-
 // function to set name
 var getPlayerName = function(){
   var name = "";
@@ -217,7 +229,14 @@ var getPlayerName = function(){
   while (name === "" || name === null){
     name = prompt("What is your robot's name");
   }
-}
+  console.log("Your robot's name is " + name);
+  return name;
+};
+
+/* END GAME FUNCTIONS */
+
+/* GAME INFORMATION / VARIABLES */
+
 // player information
 var playerInfo = {
   name: getPlayerName(),
