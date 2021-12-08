f3c7485872687e5c0797bda1711a8dc44990a3c9
Nate <natesvoboda64@gmail.com>
2021-12-07 20:29:36 -0500

    made name a required field

--- assets/js/game.js
+++ assets/js/game.js
@@ -169,9 +169,17 @@ var shop = function() {
 
 /* GAME INFORMATION / VARIABLES */
 
+// function to set name
+var getPlayerName = function(){
+  var name = "";
+
+  while (name === "" || name === null){
+    name = prompt("What is your robot's name");
+  }
+}
 // player information
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
